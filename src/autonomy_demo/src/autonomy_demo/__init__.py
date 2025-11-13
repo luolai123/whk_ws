434ec8c07a9b4eadca057460a97d287c6385551f
luolai123 <145409471+luolai123@users.noreply.github.com>
2025-11-13 23:49:08 +0800

    Fix camera pitch orientation and YOPO projection

---
+++ src/autonomy_demo/src/autonomy_demo/__init__.py
@@ -0,0 +1,37 @@
+"""Utility modules for the autonomy_demo package."""
+
+from .obstacle_field import ObstacleField
+from .safe_navigation import (
+    SafeRegion,
+    clamp_normalized,
+    compute_direction_from_pixel,
+    cubic_hermite_path,
+    find_largest_safe_region,
+    is_pixel_safe,
+    jerk_metrics,
+    jerk_score,
+    orientation_rate_score,
+    orientation_rate_stats,
+    path_smoothness,
+    project_direction_to_pixel,
+    rotate_direction,
+    sample_yopo_directions,
+)
+
+__all__ = [
+    "ObstacleField",
+    "SafeRegion",
+    "clamp_normalized",
+    "compute_direction_from_pixel",
+    "cubic_hermite_path",
+    "find_largest_safe_region",
+    "is_pixel_safe",
+    "jerk_metrics",
+    "jerk_score",
+    "orientation_rate_score",
+    "orientation_rate_stats",
+    "path_smoothness",
+    "project_direction_to_pixel",
+    "rotate_direction",
+    "sample_yopo_directions",
+]
